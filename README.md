29f9a69c902e7795e730b1d2ffefe0cffad9b383
Sooze16 <slkpes2002@yahoo.com>
2019-07-25 20:13:24 -0500

    updated CSS

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # FriendFinder
 Friend Finder - Node and Express Servers
+
+
+A compatibility-based "Friend Finder" application -- basically a dating app. This full-stack site will take in results from your users' surveys, then compare their answers with those from other users. The app will then display the name and picture of the user with the best overall match.
+
+Express is used to handle routing and the app will be depoloyed to Heroku so other users can fill it out.
+
+
+
--- app/data/data.js
+++ app/data/data.js
@@ -1,18 +1,42 @@
 var friendsData = [{
-        name: "isabel",
-        photo: "",
+        name: "Sofia",
+        photo: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimg1.southernliving.timeinc.net%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fresponsive_etr_gallery_desktop_portrait%2Fpublic%2Fimage%2F2017%2F02%2Fmain%2Fsophia_the_golden_girls-93746278.jpg%3Fitok%3D6WyjtQBA&q=85",
+        scores: [5, 4, 3, 2, 1, 5, 5, 1, 1, 1],
+    },
+
+    {
+        name: "Jim Stare",
+        photo: "https://images.pexels.com/photos/1300402/pexels-photo-1300402.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
         scores: [1, 4, 3, 2, 1, 5, 5, 1, 1, 1],
     },
 
     {
-        name: "susan",
-        photo: "",
+        name: "Justin Star",
+        photo: "https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
         scores: [1, 4, 3, 2, 1, 5, 5, 1, 1, 1],
     },
 
     {
-        name: "tom",
-        photo: "",
+        name: "Tami Bloom",
+        photo: "https://images.pexels.com/photos/1319911/pexels-photo-1319911.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        scores: [1, 4, 3, 2, 1, 5, 5, 1, 1, 1],
+    },
+
+    {
+        name: "Margaret Pine",
+        photo: "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        scores: [1, 4, 3, 2, 1, 5, 5, 1, 1, 1],
+    },
+
+    {
+        name: "Chris Witt",
+        photo: "https://images.pexels.com/photos/157741/woman-confident-happy-confident-woman-157741.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        scores: [1, 4, 3, 2, 1, 5, 5, 1, 1, 1],
+    },
+
+    {
+        name: "John Doe",
+        photo: "https://images.pexels.com/photos/936593/pexels-photo-936593.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
         scores: [1, 4, 3, 2, 1, 5, 5, 1, 1, 1],
     },
 
