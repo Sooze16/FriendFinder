726a6aa9c5993c07398a571dd5543ef3985927c6
Sooze16 <slkpes2002@yahoo.com>
2019-07-27 09:32:53 -0500

    fixed error g

--- server.js
+++ server.js
@@ -1,45 +1,51 @@
-g // / ==============================================================================
-// DEPENDENCIES
-// Series of npm packages that we will use to give our server useful functionality
-// ==============================================================================
+ // / ==============================================================================
+ // DEPENDENCIES
+ // Series of npm packages that we will use to give our server useful functionality
+ // ==============================================================================
 
-var express = require("express");
+ var express = require("express");
 
-var path = require("path");
+ var path = require("path");
 
-// ==============================================================================
-// EXPRESS CONFIGURATION
-// This sets up the basic properties for our express server
-// ==============================================================================
+ // ==============================================================================
+ // EXPRESS CONFIGURATION
+ // This sets up the basic properties for our express server
+ // ==============================================================================
 
-// Tells node that we are creating an "express" server
-var app = express();
+ // Tells node that we are creating an "express" server
+ var app = express();
 
-// Sets an initial port. We"ll use this later in our listener
-var PORT = process.env.PORT || 8080;
-
-// Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// ================================================================================
-// ROUTER
-// The below points our server to a series of "route" files.
-// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
-// ================================================================================
-
-require("./routing/apiRoutes")(app);
-require("./routing/htmlRoutes")(app);
+ // Sets an initial port. We"ll use this later in our listener
+ var PORT = process.env.PORT || 8080;
 
 
 
+ // Sets up the Express app to handle data parsing
+ app.use(express.urlencoded({ extended: true }));
+ app.use(express.json());
+ app.use(express.static(path.join(__dirname, 'public')));
 
 
-// =============================================================================
-// LISTENER
-// The below code effectively "starts" our server
-// =============================================================================
 
-app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
-});
+ //  app.use('/public', express.static(__dirname + "/public"));
+ // ================================================================================
+ // ROUTER
+ // The below points our server to a series of "route" files.
+ // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+ // ================================================================================
+
+ require("./routing/apiRoutes")(app);
+ require("./routing/htmlRoutes")(app);
+
+
+
+
+
+ // =============================================================================
+ // LISTENER
+ // The below code effectively "starts" our server
+ // =============================================================================
+
+ app.listen(PORT, function() {
+     console.log("App listening on PORT: " + PORT);
+ });
